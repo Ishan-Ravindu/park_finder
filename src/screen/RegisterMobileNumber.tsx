f01a4cff154ff30ab0f0495de84488d47f5c6626
Ishan Ravindu <ishanravindu27596@gmail.com>
2023-05-12 00:56:07 +0530

    fix ui in registermobilescreen

--- src/screen/RegisterMobileNumber.tsx
+++ src/screen/RegisterMobileNumber.tsx
@@ -2,7 +2,6 @@ import {View, Text, StyleSheet} from 'react-native';
 import MobileNumberSelection from '../components/MobileNumberSelection';
 import {PRIMARY_BACKGROUND_COLOR, PRIMARY_TEXT_COLOR} from '../styles/colors';
 import Button from '../components/Button';
-import {BOTTOM_BUTTON_BOTTOM, BOTTOM_BUTTON_LEFT} from '../styles/positions';
 import {RootStackParamList} from '../../App';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
@@ -18,8 +17,10 @@ const RegisterMobileNumber: React.FC<RegisterMobileNumberProps> = ({
 }: RegisterMobileNumberProps) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Enter your mobile number</Text>
-      <MobileNumberSelection />
+      <View style={styles.mobileNumberContainer}>
+        <Text style={styles.title}>Enter your mobile number</Text>
+        <MobileNumberSelection />
+      </View>
       <View style={styles.buttonContainer}>
         <Button
           onPress={() => {
@@ -36,16 +37,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: PRIMARY_BACKGROUND_COLOR,
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 30,
     fontWeight: '500',
     color: PRIMARY_TEXT_COLOR,
+    marginBottom: 15,
+  },
+  mobileNumberContainer: {
+    flex: 4,
   },
   buttonContainer: {
-    position: 'absolute',
-    bottom: BOTTOM_BUTTON_BOTTOM,
-    left: BOTTOM_BUTTON_LEFT,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
